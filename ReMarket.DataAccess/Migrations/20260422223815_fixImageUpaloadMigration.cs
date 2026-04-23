5d43e49fccce494e953ea1a75a7b6363f6d66322
he1322482639 <he1322482639@gmail.com>
2026-04-23 19:13:01 +1200

    Images

---
+++ ReMarket.DataAccess/Migrations/20260422223815_fixImageUpaloadMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ReMarket.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class fixImageUpaloadMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
